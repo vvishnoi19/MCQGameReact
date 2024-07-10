6279810ede06a2c576c94bb4e93cbe2a9ff94086
vishal <vvishnoi969@gmail.com>
2024-07-10 21:27:55 +0530

    again change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Question from './Components/Question.js';
 
-function App() {
+const questions = [
+  {
+    question: '1. What is the default port for a React development server?',
+    options: ['3000', '8080', '3001', '8000'],
+    answer: '3000',
+  },
+  {
+    question: '2. Which hook is used to manage state in a functional component?',
+    options: ['useState', 'useContext', 'useEffect', 'useReducer'],
+    answer: 'useState',
+  },
+  {
+    question: '3. What does JSX stand for?',
+    options: ['JavaScript XML', 'Java Syntax Extension', ' JavaScript Xtra', 'JavaX Syntax'],
+    answer: 'JavaScript XML',
+  },
+  {
+    question: '4. Which keyword is used to create a class in JavaScript?',
+    options: ['constructor', 'class', ' function', 'object'],
+    answer: 'class',
+  },
+ 
+];
+
+const App = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswer = (selectedOption) => {
+    if (selectedOption === questions[currentQuestionIndex].answer) {
+      setScore(score + 1);
+    }
+
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    if (nextQuestionIndex < questions.length) {
+      setCurrentQuestionIndex(nextQuestionIndex);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          <h2>
+            You scored {score} out of {questions.length}
+          </h2>
+        </div>
+      ) : (
+        <Question
+          question={questions[currentQuestionIndex].question}
+          options={questions[currentQuestionIndex].options}
+          onAnswer={handleAnswer}
+        />
+      )}
     </div>
   );
-}
+  
+};
 
 export default App;
+
+
+
---
+++ src/Components/Question.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Question = ({ question, options, onAnswer }) => {
+  return (
+    <div className="question-container">
+      <h2>{question}</h2>
+      <div className="options">
+        {options.map((option, index) => (
+          <button key={index} onClick={() => onAnswer(option)}>
+            {option}
+          </button>
+        ))}
+      </div>
+   
+    </div>
+  );
+};
+
+export default Question;
